5f85043eb089225b570df96a4d1ae42966acdabb
killuari <killuari@protonmail.com>
2026-07-03 23:44:35 +0200

    Add parametererklaerungen zu primitive formen

--- assets/demos/primitiv-galerie/demo.js
+++ assets/demos/primitiv-galerie/demo.js
@@ -22,29 +22,35 @@
     ];
 
     const glslRaw = [
-`float sdSphere(vec3 p, float r) {
+`// nach Quilez
+float sdSphere(vec3 p, float r) {
     return length(p) - r;
 }`,
-`float sdBox(vec3 p, vec3 b) {
+`// nach Quilez
+float sdBox(vec3 p, vec3 b) {
     vec3 q = abs(p) - b;
     return length(max(q, 0.0))
          + min(max(q.x, max(q.y, q.z)), 0.0);
 }`,
-`float sdTorus(vec3 p, vec2 t) {
+`// nach Quilez
+float sdTorus(vec3 p, vec2 t) {
     vec2 q = vec2(length(p.xz) - t.x, p.y);
     return length(q) - t.y;
 }`,
-`float sdCapsule(vec3 p, float h, float r) {
+`// nach Quilez
+float sdCapsule(vec3 p, float h, float r) {
     p.y -= clamp(p.y, -h, h);
     return length(p) - r;
 }`,
-`float sdCylinder(vec3 p, float h, float r) {
+`// nach Quilez
+float sdCylinder(vec3 p, float h, float r) {
     vec2 d = abs(vec2(length(p.xz), p.y))
            - vec2(r, h);
     return min(max(d.x, d.y), 0.0)
          + length(max(d, 0.0));
 }`,
-`float sdOctahedron(vec3 p, float s) {
+`// nach Quilez
+float sdOctahedron(vec3 p, float s) {
     p = abs(p);
     float m = p.x + p.y + p.z - s;
     vec3 q;
